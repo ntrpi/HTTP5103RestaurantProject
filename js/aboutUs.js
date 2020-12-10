d57c83e635828c66ffc4d5fec2d5dbdbaaeb3c99
diarrabak <bakary.diarra9@gmail.com>
2020-12-10 16:36:22 +0000

    Some comments

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,14 +1,22 @@
 window.onload=aboutUs;
 
 function aboutUs(){
+	//Element containing the two images
 	var imgContainer=document.getElementById("specials");
+	//Links parent of the images
 	var links=imgContainer.getElementsByTagName("a");
+	//Paragraph telling the history of recipe 1
 	var description=document.getElementById("description");
+	//Paragraph telling the history of recipe 2
 	var description1=document.getElementById("description1");
+	
+	//Hovering the mouse on the image gives the description and removing the mouse hides the description
     links[0].onmouseover=displayDescript;
 	links[1].onmouseover=displayDescript1;
 	links[0].onmouseout=hideDescript;
 	links[1].onmouseout=hideDescript1;
+	
+	//Function describing the first image
 	function displayDescript(){
 		description.style.display="block";
 		description.style.backgroundColor="black";
@@ -21,10 +29,13 @@ function aboutUs(){
 		description.style.padding="10px";
 		description.style.fontFamily="miriam libre";
 	}
+	
+	//This function hides the description of the first image
 	function hideDescript(){
 		description.style.display="none";
 	}
 	
+	//Function describing the second image
 	function displayDescript1(){
 		description1.style.display="block";
 		description1.style.backgroundColor="black";
@@ -37,6 +48,8 @@ function aboutUs(){
 		description1.style.padding="10px";
 		description.style.fontFamily="miriam libre";
 	}
+	
+	//This function hides the description of the second image
 	function hideDescript1(){
 		description1.style.display="none";
 	}
