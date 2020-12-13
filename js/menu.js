b5307abfb30cedb9033a1d763c89a6eed168a565
Alexis Arevalo <alexgian1997@gmail.com>
2020-12-12 23:17:41 -0500

    Update Locations

--- js/menu.js
+++ js/menu.js
@@ -5,28 +5,33 @@ window.onload = goNow;
 function goNow(){
 
     //GET DOM ELEMENTS WE'LL NEED
+    var liMain = document.getElementById('mainLI');
     var liAppetizer = document.getElementById('appetizersLI');
     var liSalad = document.getElementById('saladLI');
     var liDessert = document.getElementById('dessertsLI');
     var liDrink = document.getElementById('drinksLI');
 
     //SETUP LISTENERS
+    liMain.onclick = scrollDown;
     liAppetizer.onclick = scrollDown1;
     liSalad.onclick = scrollDown2;
     liDessert.onclick = scrollDown3;
     liDrink.onclick = scrollDown4;
 
     //CREATE THE FUNCTIONS WE'LL NEED
+    function scrollDown(){
+        window.scroll({ top:532, left:0, behavior: 'smooth'});
+    };
     function scrollDown1(){
-        window.scrollBy(0, 2200)
+        window.scroll({ top:2200, left:0, behavior: 'smooth'});
     };
     function scrollDown2(){
-        window.scrollBy(0, 3230)
+        window.scroll({top:3230, left:0});
     };
     function scrollDown3(){
-        window.scrollBy(0, 3800)
+        window.scroll({top:3800, left:0});
     };
     function scrollDown4(){
-        window.scrollBy(0, 5000)
+        window.scroll({top:5000, left:0});
     };
 }
